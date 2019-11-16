0069e01b518bb52bc63030939e2cb3bc85633f04
karithomas <kari.k.thomas8@gmail.com>
2019-11-16 11:02:53 -0500

    adding all calendar files

---
+++ public/js/calendar.js
@@ -0,0 +1,7 @@
+  $(document).ready(function () {
+  $('#calendar').fullCalendar({
+    googleCalendarApiKey: 'AIzaSyBoOo_mR2pvwPhsS97z73H-m-_dko9opFI',
+    events: {
+      googleCalendarId: 'lucylaneproject2@group.calendar.google.com'
+    }
+  });
