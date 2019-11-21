3304bfbece512ca4a07b4f50425a496b435bdfac
karithomas <kari.k.thomas8@gmail.com>
2019-11-21 18:35:29 -0500

    adding calendar info

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,7 +1,7 @@
-  $(document).ready(function () {
+$(function() {
   $('#calendar').fullCalendar({
     googleCalendarApiKey: 'AIzaSyBoOo_mR2pvwPhsS97z73H-m-_dko9opFI',
-    events: {
-      googleCalendarId: 'lucylaneproject2@group.calendar.google.com'
+      events: {
+    googleCalendarId: 'lucylaneproject2@group.calendar.google.com'
     }
-  });
+  }});
